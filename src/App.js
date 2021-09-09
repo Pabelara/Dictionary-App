eaffe47c8435f062d7d1b6799670b72f989ee7a0
Pabelara <80460781+Pabelara@users.noreply.github.com>
2021-09-09 14:12:29 +0100

    Created Search Form

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <Dictionary />
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
