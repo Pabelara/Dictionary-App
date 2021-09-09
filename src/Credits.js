5a96e9bcf9b70d4a58d011116ca1b0539bd60e2d
Pabelara <80460781+Pabelara@users.noreply.github.com>
2021-09-09 23:19:09 +0100

    Fixed Credit.js Link

--- src/Credits.js
+++ src/Credits.js
@@ -3,7 +3,7 @@ export default function Credits() {
     <div className="Credits">
       <p className="credits">
         This project was coded by Paula Belloch. It is{" "}
-        <a href="https://app.netlify.com/teams/pabelara/sites">open-sourced</a>{" "}
+        <a href="https://github.com/Pabelara/Dictionary-App">open-sourced</a>{" "}
         and is hosted on{" "}
         <a href="https://mystifying-jackson-c5bed8.netlify.app/">Netlify</a>
       </p>
